9bdf9c8a467e5c35f20887642adca46041557752
Zoe Meers <zoe.m@canva.com>
2023-01-24 09:09:08 +1000

    update pyrdocs

--- R/convert.R
+++ R/convert.R
@@ -20,48 +20,45 @@ pyrdocs_convert <- function(package_source_folder = here::here(),
                             reference_examples_not_run = FALSE,
                             reference_output = "md",
                             reference_qmd_options = NULL,
-                            reference_template = system.file("function_child_reference.md", package = "pyrdocs"),
                             build_parent_and_child_reference_pages = TRUE,
-                            reference_template_parent = system.file("function_parent_reference.md", package = "pyrdocs"),
-                            reference_template_child = system.file("function_child_reference.md", package = "pyrdocs"),
+                            reference_template_parent = system.file("templates/function_parent_reference.qmd", package = "pyrdocs"),
+                            reference_template_child = system.file("templates/function_child_reference.qmd", package = "pyrdocs"),
+                            reference_template = system.file("templates/function_child_reference.qmd", package = "pyrdocs"),
                             commit = c("latest_tag", "latest_commit"),
                             package_description = NULL
 ){
 
-  r_package_path <- paste0(package_source_folder, "/", r_sub_folder)
-  python_package_path <- paste0(package_source_folder, "/", python_sub_folder)
+  if(!fs::dir_exists(fs::path(r_sub_folder, quarto_sub_folder))){
+    fs::dir_create(fs::path(r_sub_folder,quarto_sub_folder))
+  }
 
   ## generate R docs
-  ecodown::ecodown_convert(package_source_folder = r_package_path,
+  ecodown::ecodown_convert(package_source_folder = r_sub_folder,
                            branch = branch,
-                           quarto_folder = r_package_path,
+                           quarto_folder = r_sub_folder,
                            quarto_sub_folder = quarto_sub_folder,
                            reference_folder = reference_folder,
-                           r_reference_folder = r_reference_folder,
-                           python_reference_folder = python_reference_folder,
                            reference_output = reference_output,
                            build_parent_and_child_reference_pages = build_parent_and_child_reference_pages,
                            reference_template = reference_template,
                            reference_template_parent = reference_template_parent
   )
-  ecodown::ecodown_build(quarto_folder = paste0(r_package_path, "/", quarto_sub_folder))
 
-  setwd(path='..')
-  if (!dir.exists(quarto_sub_folder)) {
-    dir.create(path = quarto_sub_folder)
-  }
-  file.move(paste0(r_package_path, quarto_sub_folder), quarto_sub_folder)
+  md_files <- fs::dir_ls(fs::path(r_sub_folder, quarto_sub_folder, reference_folder), glob = "*.md")
+  md_files <- md_files[md_files != paste0(fs::path(r_sub_folder, quarto_sub_folder, reference_folder), 'index.md')]
+  r_md_files <- sub(".md", "_r.md", md_files)
+  file.rename(md_files, new_names)
+
+  setwd(dirname(here::here()))
+
+  fs::dir_copy(fs::path(r_sub_folder, quarto_sub_folder), fs::path(quarto_sub_folder))
+
   ## generate python docs
-  if(!dir.exists(paste0(quarto_folder, quarto_sub_folder, reference_folder, python_reference_folder, collapse = "/"))){
-    dir.create(paste0(quarto_folder, quarto_sub_folder, reference_folder, collapse = "/"))
-    dir.create(paste0(quarto_folder, quarto_sub_folder, reference_folder, python_reference_folder, collapse = "/"))
-  }
-  generate_python_md_modules(python_package_path, "canviz", reference_folder, python_reference_folder)
+  generate_python_md_modules(python_package_path, "canviz", reference_folder)
   split_and_clean_python_md_modules(python_package_path,
                                     "canviz",
                                     quarto_folder = package_source_folder,
                                     quarto_sub_folder = quarto_sub_folder,
-                                    reference_folder = reference_folder,
-                                    python_reference_folder = python_reference_folder)
+                                    reference_folder = reference_folder)
 
 }
